50acc71a860da2444e095fcee12604527639219e
yenvietsoft <128137927+binhkid2@users.noreply.github.com>
2024-03-18 08:02:17 +0000

    add react-store front

--- src/pages/CategoryPage/index.tsx
+++ src/pages/CategoryPage/index.tsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { SfSelect } from '@storefront-ui/react';
+import BackHeader from "../../components/BackHeader";
 
+const options = [
+  { label: 'Relevance', value: 'relevance' },
+  { label: 'Price: Low to High', value: 'price low to high' },
+  { label: 'Price: High to Low', value: 'price high to low' },
+  { label: 'New Arrivals', value: 'new arrivals' },
+  { label: 'Customer Rating', value: 'customer rating' },
+  { label: 'Bestsellers', value: 'bestsellers' },
+];
 const mockdata: {
   title: string;
   url:string
@@ -28,7 +38,8 @@ export default function CategoryPage() {
     }
   return (
     <>
-      <div className="grid grid-rows-1 grid-flow-col gap-2 mx-auto scroll-smooth overflow-auto hide-scrollbar  rounded-lg border border-gray-100 bg-gray-100 p-1">
+    <BackHeader/>
+      <div className=" grid grid-rows-1 grid-flow-col gap-2 mx-auto scroll-smooth overflow-auto hide-scrollbar  rounded-lg border border-gray-100 bg-gray-100 p-1">
         {mockdata.map((item, index) => (
           <button
             key={index} // You should use a unique key for each item when rendering lists in React
@@ -39,7 +50,27 @@ export default function CategoryPage() {
           </button>
         ))}
       </div>
-     
+      <div className="w-full md:max-w-[376px]">
+      <p className="bg-neutral-100  w-full py-2 pl-4 rounded uppercase  font-bold tracking-widest">
+        Sort by
+      </p>
+      <div className="px-4 flex gap-2">
+        <SfSelect aria-label="Sort by">
+          {options.map((option) => (
+            <option value={option.value} key={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </SfSelect>
+        <SfSelect aria-label="Sort by">
+          {options.map((option) => (
+            <option value={option.value} key={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </SfSelect>
+      </div>
+    </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+// tailwind.config.js
+import { tailwindConfig } from '@storefront-ui/react/tailwind-config';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  presets: [tailwindConfig],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@storefront-ui/react/**/*.{js,mjs}'],
   theme: {
     extend: {},
   },
