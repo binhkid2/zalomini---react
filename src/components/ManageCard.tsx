1511391f42dceef4c6a7fdd492d2d8e4c363563c
yenvietsoft <128137927+binhkid2@users.noreply.github.com>
2024-03-26 13:19:59 +0000

    FontAwesomeIcon

--- src/components/ManageCard.tsx
+++ src/components/ManageCard.tsx
@@ -1,24 +1,24 @@
 
-import {  SfLink, SfIconDelete } from '@storefront-ui/react';
+import { SfLink } from '@storefront-ui/react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons/faEye";
 import { FC, Suspense } from 'react';
-import { faPenNib } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons/faEllipsis';
+import { SfDropdown, useDisclosure } from '@storefront-ui/react';
+import { faPen } from '@fortawesome/free-solid-svg-icons/faPen';
 
 
   const ManageCardContent: FC = () => {
+    const { isOpen, toggle, close } = useDisclosure();
     return (
         <>
-       <div className='border hover:shadow-lg min-w-[320px] max-w-[640px] p-1 m-4'>
+       <div className='border hover:shadow-lg  p-1 m-4'>
        <div className="relative flex ">
-        <div className="relative overflow-hidden rounded-md w-[100px] sm:w-[176px]">
+        <div className="relative overflow-hidden rounded-md ">
           <SfLink href="#">
             <img
-              className="w-full h-auto border rounded-md "
+              className="w-36 md:w-48 h-36 md:h-48 object-cover border rounded-md "
               src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/smartwatch.png"
               alt="alt"
-              width="300"
-              height="300"
             />
           </SfLink>
           <p className="absolute top-0 left-0 z-10 whitespace-nowrap bg-yellow-400 px-3 py-1 text-xs font-medium">
@@ -28,16 +28,16 @@ import { faPenNib } from '@fortawesome/free-solid-svg-icons';
         <div className="flex flex-col pl-4 min-w-[180px] flex-1">
           <div className="flex justify-between">
           <SfLink href="#" variant="secondary" className="no-underline typography-text-sm sm:typography-text-lg font-bold">
-            Smartwatch Fitness Tracker
+            Smartwatch Fitness Tracker Smartwatch Fitness Tracker
           </SfLink>
-          <button
-                aria-label="Remove"
-                type="button"
-                className="text-neutral-500 text-xs font-light ml-auto flex items-center px-3 py-1.5"
-              >
-               <FontAwesomeIcon icon={faEye} size="lg" />
-                <span className="hidden ml-1.5 sm:block"> Hide </span>
-              </button>
+          
+              <SfDropdown trigger={<button className='mr-2' onClick={toggle}><FontAwesomeIcon icon={faEllipsis} size="lg" style={{color: "#04711a",}} /></button>} open={isOpen} onClose={close}>
+      <ul className=" p-2 rounded bg-gray-100 w-max">
+        <li className='h-5 ' onClick={()=>{console.log("Hide")}}>Ẩn tin</li>
+        <li className='h-5 ' onClick={()=>{console.log("delete")}}>Xóa tin</li>
+        <li className='h-5 ' onClick={()=>{console.log("Pro")}}>Nâng cấp lên pro</li>
+      </ul>
+    </SfDropdown>
           
           </div>
           <div className="my-2 sm:mb-0">
@@ -53,21 +53,20 @@ import { faPenNib } from '@fortawesome/free-solid-svg-icons';
             </ul>
           </div>
           <div className="items-center sm:mt-auto sm:flex">
-            <span className="font-bold sm:ml-auto sm:order-1 typography-text-sm sm:typography-text-lg">$2,345.99</span>
+            <span className="font-bold sm:ml-auto sm:order-1 typography-text-sm sm:typography-text-lg mr-3">$2,345.99</span>
             <div className="flex items-center justify-between mt-4 sm:mt-0">
-              <button
-                aria-label="Remove"
+            <button
+                aria-label="edit"
                 type="button"
                 className="text-neutral-500 text-xs font-light ml-auto flex items-center px-3 py-1.5"
               >
-                <SfIconDelete />
-                <span className="hidden ml-1.5 sm:block"> Remove </span>
+                <FontAwesomeIcon icon={faPen} size="lg" />
+                <span className="hidden ml-1.5 sm:block"> Edit </span>
               </button>
             </div>
           </div>
         </div>
       </div>
-      <div className='flex justify-center px-2'><button>Edit <FontAwesomeIcon icon={faPenNib} size="lg" /></button></div>
        </div>
         </>
     )
