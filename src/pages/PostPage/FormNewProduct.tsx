6036e86ca7884446485106edd2794b07b44fb88b
binhkid2 <binhkid2@gmail.com>
2024-04-04 18:21:07 +0700

    ass

--- src/pages/PostPage/FormNewProduct.tsx
+++ src/pages/PostPage/FormNewProduct.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
-import { SfInput, SfButton } from "@storefront-ui/react";
-import { useAtomValue,useAtom } from "jotai";
+import { SfInput, SfButton, SfThumbnail } from "@storefront-ui/react";
+import { useAtomValue, useAtom } from "jotai";
 import { categoryAtom, imagesAtom, shopAtom } from "../../utils/store";
 import axios from "axios";
 import { useSnackbar } from "notistack";
 import { useNavigate } from "react-router-dom";
-import { ConvertToImagesJSON,Images } from "../../utils/ConvertToImagesJSON";
+import { ConvertToImagesJSON, Images } from "../../utils/ConvertToImagesJSON";
+
+//tags
+import { SfChip, SfIconCloseSm } from "@storefront-ui/react";
+interface Variant {
+  variant: string;
+}
+const initVariants: Variant[] = [];
+
+interface Tag {
+  tag: string;
+}
+const initTags: Tag[] = [];
 export default function FormNewProduct() {
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
@@ -17,15 +29,44 @@ export default function FormNewProduct() {
   const [wholesalePrice, setWholesalePrice] = useState(0);
   const [moq, setMoq] = useState(0);
   const [notice, setNotice] = useState("");
-  //TODO
-  const variants = ["variants do it later useState"];
-  const tags = ["tags do it later useState"];
-//Get from Atom
-const [base64Urls, setBase64Urls] = useAtom(imagesAtom)
-const shop = useAtomValue(shopAtom)._id;
-const shopName = useAtomValue(shopAtom).name;
-const address = useAtomValue(shopAtom).address;
-const category = useAtomValue(categoryAtom);
+  //variants
+  const [variants, setVariants] = useState(initVariants);
+  const removeVariants = (val: string) => {
+    const newList = variants.filter((item) => item.variant !== val);
+    setVariants(newList);
+  };
+
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setVariants([...variants, { variant: inputValue.trim() }]);
+      setInputValue(""); // Clear input field after adding
+    }
+  };
+
+//tags
+  const [tags, setTags] = useState(initTags);
+  const removeTag = (val: string) => {
+    const newList = tags.filter((item) => item.tag !== val);
+    setTags(newList);
+  };
+
+  const [inputValueTag, setInputValueTag] = useState<string>("");
+
+  const handleInputKeyPressTag = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && inputValueTag.trim() !== "") {
+      setTags([...tags, { tag: inputValueTag.trim() }]);
+      setInputValueTag(""); // Clear input field after adding
+    }
+  };
+
+  //Get from Atom
+  const [base64Urls, setBase64Urls] = useAtom(imagesAtom);
+  const shop = useAtomValue(shopAtom)._id;
+  const shopName = useAtomValue(shopAtom).name;
+  const address = useAtomValue(shopAtom).address;
+  const category = useAtomValue(categoryAtom);
   //alway send like this
   const review = [
     {
@@ -59,7 +100,7 @@ const category = useAtomValue(categoryAtom);
         console.error("Error:", error);
       }
     }
-    let images:Images[] = ConvertToImagesJSON(imagesArray)
+    let images: Images[] = ConvertToImagesJSON(imagesArray);
     console.log(images);
     //use those image urls
     const dataSend = {
@@ -92,22 +133,39 @@ const category = useAtomValue(categoryAtom);
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Product Created successfully", { variant: "success" });
-        setName(''),setNotice(''),setDescription('') ,setPrice(0),setQuantity(0),setWholesalePrice(0),setMoq(0),setBase64Urls([]),images=[]
-        navigate('/')
+        setName(""),
+          setNotice(""),
+          setDescription(""),
+          setPrice(0),
+          setQuantity(0),
+          setWholesalePrice(0),
+          setMoq(0),
+          setBase64Urls([]),
+          (images = []);
+        navigate("/");
       })
       .catch((error) => {
         setLoading(false);
         enqueueSnackbar("Error when creating product", { variant: "error" });
         console.log(error);
-        setName(''),setNotice(''),setDescription('') ,setPrice(0),setQuantity(0),setWholesalePrice(0),setMoq(0),setBase64Urls([]),images=[]
-        navigate('/')
+        setName(""),
+          setNotice(""),
+          setDescription(""),
+          setPrice(0),
+          setQuantity(0),
+          setWholesalePrice(0),
+          setMoq(0),
+          setBase64Urls([]),
+          (images = []);
+        navigate("/");
       });
   };
   return (
     <>
-      <form onSubmit={sendForm} className="px-4">
+      {/*   form onSubmit={sendForm} */}
+      <div className="px-4">
         <label>
-          <span className="typography-label-sm font-medium">
+          <span className="typography-label-sm font-medium ">
             Name <span className="text-xs text-red-500"> *</span>
           </span>
           <SfInput
@@ -125,7 +183,7 @@ const category = useAtomValue(categoryAtom);
   */}
 
         <label>
-          <span className="typography-label-sm font-medium">
+          <span className="block mt-1 typography-label-sm font-medium">
             Price <span className="text-xs text-red-500"> *</span>
           </span>
           <SfInput
@@ -138,7 +196,7 @@ const category = useAtomValue(categoryAtom);
         </label>
 
         <label>
-          <span className="mt-2 typography-label-sm font-medium block">
+          <span className="mt-1 typography-label-sm font-medium block">
             Description <span className="text-xs text-red-500"> *</span>
           </span>
           <textarea
@@ -149,9 +207,43 @@ const category = useAtomValue(categoryAtom);
             className="block w-full py-2 pl-4 pr-7 rounded-md border border-neutral-300 placeholder:text-neutral-500"
           />
         </label>
+        {/*  Variants */}
+        <label>
+          <span className="block mt-1 typography-label-sm font-medium">
+            Variants
+          </span>
+          <SfInput
+            value={inputValue}
+            placeholder="Enter a variant"
+            className=""
+            onKeyDown={handleInputKeyPress}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+        </label>
+        <ul className="flex flex-wrap gap-4 sm:flex-row my-2">
+          {variants.map(({ variant }) => (
+            <li key={variant}>
+              <SfChip
+                className="mr-2 rounded-lg"
+                inputProps={{
+                  checked: true,
+                  onChange: () => removeVariants(variant),
+                }}
+                slotSuffix={
+                  <SfIconCloseSm className="text-neutral-500 hover:text-primary-800 active:text-primary-900 disabled:opacity-20" />
+                }
+              >
+                {variant}
+              </SfChip>
+            </li>
+          ))}
+        </ul>
+
 
         <label>
-          <span className="typography-label-sm font-medium">Quantity</span>
+          <span className="block mt-1 typography-label-sm font-medium">
+            Quantity
+          </span>
           <SfInput
             value={quantity}
             placeholder="quantity"
@@ -162,7 +254,7 @@ const category = useAtomValue(categoryAtom);
         </label>
 
         <label>
-          <span className="typography-label-sm font-medium">
+          <span className="block mt-1 typography-label-sm font-medium">
             wholesalePrice
           </span>
           <SfInput
@@ -177,7 +269,9 @@ const category = useAtomValue(categoryAtom);
         </label>
 
         <label>
-          <span className="typography-label-sm font-medium">moq</span>
+          <span className="block mt-1 typography-label-sm font-medium">
+            moq
+          </span>
           <SfInput
             value={moq}
             placeholder="moq"
@@ -188,7 +282,10 @@ const category = useAtomValue(categoryAtom);
         </label>
 
         <label>
-          <span className="typography-label-sm font-medium"> notice </span>
+          <span className="block mt-1 typography-label-sm font-medium">
+            {" "}
+            notice{" "}
+          </span>
           <SfInput
             value={notice}
             placeholder="notice"
@@ -196,15 +293,57 @@ const category = useAtomValue(categoryAtom);
             onChange={(event) => setNotice(event.target.value)}
           />
         </label>
+
+ {/*  Tags */}
+ <label>
+          <span className="block mt-1 typography-label-sm font-medium">
+            Tags
+          </span>
+          <SfInput
+            value={inputValueTag}
+            placeholder="Enter a variant"
+            className=""
+            onKeyDown={handleInputKeyPressTag}
+            onChange={(event) => setInputValueTag(event.target.value)}
+          />
+        </label>
+        <ul className="flex flex-wrap gap-1 sm:flex-row ">
+          {tags.map(({ tag }) => (
+            <li key={tag}>
+              <SfChip
+                className=" rounded-lg"
+                inputProps={{
+                  checked: true,
+                  onChange: () => removeTag(tag),
+                }}
+                slotPrefix={<SfThumbnail className={`bg-gray-500`} />}
+                slotSuffix={
+                  <SfIconCloseSm className="text-neutral-500 hover:text-primary-800 active:text-primary-900 disabled:opacity-20" />
+                }
+              >
+                {tag}
+              </SfChip>
+            </li>
+          ))}
+        </ul>
+
+
+
+
         <p className="text-xs text-neutral-500 typography-text-sm mt-2">
           * marked fields are required
         </p>
         <div className="flex gap-x-4 md:justify-end mt-6">
-          <SfButton type="submit" className="flex-grow md:flex-grow-0" disabled={loading}>
+          <SfButton
+            type="submit"
+            className="flex-grow md:flex-grow-0"
+            disabled={loading}
+            onClick={sendForm}
+          >
             Submit
           </SfButton>
         </div>
-      </form>
+      </div>
     </>
   );
 }
